d2e94ce2f130ff0a83753cf08d81c260ea3eaeec
zainidineafi <firmanmangkuraja@gmail.com>
2024-08-24 12:07:48 +0700

    Migrasi Tabel Baru

---
+++ app/Database/Migrations/2024-08-24-050325_TbBerita.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbBerita extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_berita' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'judul_berita' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => false,
+            ],
+            'foto_berita' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'deskripsi_berita' => [
+                'type'       => 'TEXT',
+                'null'       => true,
+            ],
+            'tags_berita' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'datetime_berita' => [
+                'type'       => 'DATETIME',
+                'null'       => false,
+            ],
+        ]);
+
+        $this->forge->addKey('id_berita', true);
+        $this->forge->createTable('tb_berita');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_berita');
+    }
+}
